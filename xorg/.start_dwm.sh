f009589f6e6a15c1ce8443025030f7c54eb2fa4c
manojkur <manoj.kurapati@gmail.com>
2020-03-07 22:34:27 -0500

    added multiple monitor support and disabled screen timeout in xorg

--- xorg/.start_dwm.sh
+++ xorg/.start_dwm.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# setup background changing systemd timer
-# systemd-run --on-calendar='*:0/5' --user --unit background_wallpaper.service --service-type=simple -- /usr/bin/feh --no-fehbg --bg-fill --randomize -recursive ~/dotfiles/backgrounds/backgrounds/
+# start dwm
 
+xset -dpms
+xrandr
+xrandr --output HDMI1 --primary --auto --output DP2 --auto --left-of HDMI1 --output eDP1 --auto --left-of DP2
 /usr/bin/feh --no-fehbg --bg-fill --randomize -recursive ~/dotfiles/backgrounds/backgrounds/
 
-# start dwm
 while xsetroot -name "`date` `uptime | sed 's/.*,//'`"
 do
     sleep 1 
-        
-#    if [[ $(( $(date +"%M" | sed 's/^0*//') % 5 )) == 0 ]] && [[ $(( $(date +"%S") | sed 's/^0*//' == 0 )) ]] 
-#    then
-#        /usr/bin/feh --no-fehbg --bg-fill --randomize -recursive ~/dotfiles/backgrounds/backgrounds/
-#    fi
 done &
 while /usr/bin/feh --no-fehbg --bg-fill --randomize -recursive ~/dotfiles/backgrounds/backgrounds/
 do
