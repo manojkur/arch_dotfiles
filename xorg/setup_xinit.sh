20722732f111b6002f311e8abf7a138b4fb4b800
manojkur <manoj.kurapati@gmail.com>
2020-03-09 15:04:17 -0400

    removed screensaver and added caps to esc keybinding

--- xorg/setup_xinit.sh
+++ xorg/setup_xinit.sh
@@ -11,17 +11,20 @@ userdir="$HOME"
 
 echo -e "--> Moving all existing files to *.old"
 mv $userdir/.xinitrc $userdir/.xinitrc.old > /dev/null 2>&1
+mv $userdir/.Xmodmap $userdir/.Xmodmap.old > /dev/null 2>&1
 mv $userdir/.xorg $userdir/.xorg.old > /dev/null 2>&1
 
 echo -e "--> Linking files to repository"
 mkdir -p $userdir/.xorg
 ln -s $PWD/.xinitrc $userdir/.xinitrc
+ln -s $PWD/.Xmodmap $userdir/.Xmodmap
 ln -s $PWD/status_functions.sh $userdir/.xorg/status_functions.sh
 
 if [ "$1" == "-d" ]
 then
     echo -e "--> Deleting old files"
     rm -f $userdir/.xinitrc.old
+    rm -f $userdir/.Xmodmap.old
     rm -f $userdir/.xorg.old
 fi
 
