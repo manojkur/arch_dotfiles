1dc40a21dd1ec0732819c26375235b9d73eaf5e1
manojkur <manoj.kurapati@gmail.com>
2022-01-19 23:21:53 -0600

    removed startup commands

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get Keychron K2 keyboard to work with Linux
-echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode
+# echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode
 
 # Remap CAPS to ESC
-setxkbmap -option caps:escape
+# setxkbmap -option caps:escape
